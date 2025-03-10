19819651d7eac2ab6cbe164d5648aa666934a408
ericlyndert <mananilyndert2@gmail.com>
2025-03-10 09:17:44 +0200

    Update date.tsx

--- src/components/menu/nav/date.tsx
+++ src/components/menu/nav/date.tsx
@@ -1,12 +1,14 @@
 // data.ts
 export const navLinks = [
     { path: "/", label: "Home" },
-    { path: "/events", label: "Events" },
   { path: "/about", label: "About Us" },
-  { path: "/", label: "Programs" },
+     {path:"https://youth-dashbord-ig9h.onrender.com/", label:"Dashboard"},
+    { path: "/events", label: "Events" },
+
+  { path: "/News", label: "Programs" },
   { path: "/volunteer", label: "Volunteer" },
     { path: "/News", label: "News & Stories" },
-  
+   
   ];
   
   
