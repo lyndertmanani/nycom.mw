3d1193ac602440183d245c2e16aa85e1cc9b3cd6
ericlyndert <mananilyndert2@gmail.com>
2025-03-10 09:18:11 +0200

    Update date.tsx

--- src/components/menu/nav/date.tsx
+++ src/components/menu/nav/date.tsx
@@ -4,7 +4,6 @@ export const navLinks = [
   { path: "/about", label: "About Us" },
      {path:"https://youth-dashbord-ig9h.onrender.com/", label:"Dashboard"},
     { path: "/events", label: "Events" },
-
   { path: "/News", label: "Programs" },
   { path: "/volunteer", label: "Volunteer" },
     { path: "/News", label: "News & Stories" },
